4991d9645ca6192754d2bfdf59c18f756e301523
Tomas Polach <tomas@polach.ch>
2021-05-24 15:07:40 +0200

    Fake query might take up to 900 ms

--- src/model/get-data.js
+++ src/model/get-data.js
@@ -1,13 +1,13 @@
 const { wait } = require('../util/wait.js');
 
 const WAIT_MIN = 100;
-const WAIT_MAX = 100;
+const WAIT_MAX = 900;
 
 const counters = {};
 async function fakeDbQuery(str) {
   await wait(Math.round(Math.random() * (WAIT_MAX - WAIT_MIN)) + WAIT_MIN);
   counters[str] = counters[str] === undefined ? 1 : counters[str] + 1;
-  return str + counters[str]
+  return str + counters[str];
 }
 
 module.exports = {
